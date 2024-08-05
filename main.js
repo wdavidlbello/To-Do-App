771f220770d80db983db44f73fa10e49a49388ef
wdavidlbello <wdavidlbello@gmail.com>
2024-08-04 20:29:15 -0500

    event click add task

--- main.js
+++ main.js
@@ -2,20 +2,26 @@ const boton = document.querySelector('button')
 const input = document.querySelector('input')
 const ul = document.querySelector('ul')
 
-function onClick(){
+function onClick(event){
+    event.preventDefault();
     const li = document.createElement('li')
     const p = document.createElement('p')
     const checked = document.createElement('img')
     const deletebtn = document.createElement('button')
     checked.setAttribute('src','./images/unchecked.png')
     checked.setAttribute('class','checkedIcon')
-    p.textContent = "Tarea adicionada"
+    p.textContent = input.value
     deletebtn.textContent ='x'
     deletebtn.setAttribute('class','li-button')
     li.appendChild(checked)
     li.appendChild(p)
     li.appendChild(deletebtn)
     ul.appendChild(li)
+
+    // Add event listener to the delete button
+    deletebtn.addEventListener('click', function() {
+        ul.removeChild(li);
+    });
 }
 
-boton.addEventListener("click",onClick);
+boton.addEventListener("click",onClick)
